a2fd7822f77d7ac10ea2acfab709462ea6718dcb
coco-toyp <o.r.191112@gmail.com>
2020-12-16 21:36:22 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :password
+  end
+  
 end
